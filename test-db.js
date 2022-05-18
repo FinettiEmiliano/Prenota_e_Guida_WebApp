543f70243c012adf042f1d3c5ee5cadcb004335f
Diego <diego.arrondo@studenti.unitn.it>
2022-05-18 13:54:34 +0200

    Update test-db.js

--- test-db.js
+++ test-db.js
@@ -7,15 +7,15 @@ const insertUser = async(obj)=>{
     //console.log("Utente inserito");
 }
 
-//funzione per cercare un utente dal user_name dato il nameutente
+//funzione per cercare un utente dal username dato il nameutente
 const seachUser = async(usr)=>{ //usr -> name utente come parametro
-    const user = await ModelUtente.findOne({user_name: usr});
+    const user = await ModelUtente.findOne({username: usr});
     console.log("Utente trovato:" +user);
 }
 
-//funzione per controllare se esiste un utente con determinati user_name e password
+//funzione per controllare se esiste un utente con determinati username e password
 const checkLogin = async(usr,ps)=>{
-    const user = await ModelUtente.findOne({user_name: usr});
+    const user = await ModelUtente.findOne({username: usr});
     if(user==null)
         console.log("Non esiste nessun "+ usr);
     else{
@@ -28,16 +28,16 @@ const checkLogin = async(usr,ps)=>{
 
 //funzione per eliminare un utente dato un obj (di tipo ModelUtente?) 
 const deleteUtente = async(usr)=>{
-    var bool = await ModelUtente.exists({user_name: usr});
+    var bool = await ModelUtente.exists({username: usr});
     if(bool){
-        const user = await ModelUtente.deleteOne({user_name: usr});
+        const user = await ModelUtente.deleteOne({username: usr});
         console.log("Utente eliminato");
     }
     else
         console.log("Non esiste tale Utente");
 }
 
-/*
+
 require("dotenv").config()  //forse non va qua ma se non lo metto non funziona, serve per l'URL contenuto in .env
 //URL: mongodb+srv://User_utentiDB:<password>@utentidb.g39cb.mongodb.net/?retryWrites=true&w=majority
 
@@ -47,37 +47,38 @@ mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology:
 
 // collezione / tabella di utenti
 const utentiSchema = new mongoose.Schema({      
-    user_name: String, 
+    username: String, 
     password: String,
-    type_user: String,    //sarebbe enum
+    user_type: String,    //sarebbe enum
     name: String,
     surname: String,
     photo: String            // come??
 });
 
-const ModelUtente = mongoose.model("Utenti", utentiSchema);  
+const ModelUtente = mongoose.model("Users", utentiSchema);  
 
 //inserimento a mano
 insertUser({
-    user_name: 'MarioDraghi', 
-    password: 'mandarino00',
-    type_user: 'Istruttore',    //sarebbe enum
+    username: 'Emi', 
+    password: 'pollo00',
+    user_type: 'Istruttore',    //sarebbe enum
     name: 'Mario',
     surname: 'Draghi',
     photo: 'pathphoto'                // come??
 })
 
 //creazione di un Utente
+/*
 const Persona = new ModelUtente({
-    user_name: 'DiegoARRONDO',
+    username: 'DiegoARRONDO',
     password: 'Cavallo00',
-    type_user: 'Istruttore',    //sarebbe enum
+    user_type: 'Istruttore',    //sarebbe enum
     name: 'Luca',
     surname: 'Abbelli',
     photo: 'pathphoto' // come??
 })
-
 */
+
 
 
 
