e9823a43f827a35f5263a1684f85e8713095e3d0
FinettiEmiliano <emiliano.finetti@studenti.unitn.it>
2022-05-20 13:00:13 +0200

    fixed UI - closed login

--- static/loginPage/script.js
+++ static/loginPage/script.js
@@ -21,17 +21,50 @@ function login()
         loggedUser.id = data.id;
         loggedUser.user_type = data.user_type;
 
-        console.log(data);
-        if(!data.success)       //check if there is an error in the credentials
-            showError(data); 
+        //check if there is an error in the credentials
+        if(!data.success)
+            showError();
+        else
+            userRole(data); 
         return;
     })
     .catch( error => console.error(error) ); // If there is any error you will catch them here
 };
 
 //function to show the credentials error 
-function showError(result){
-    //console.log(result.success);
-        document.getElementById("IdErrori").innerHTML = 
-        "<h3>Nome utente errato o inesistente</h3>";
+function showError(){
+    //delete precedent error
+    if(document.getElementById("ErrorDiv")!=null)
+    document.getElementById("ErrorDiv").remove();
+
+    //creation of new page element
+    var div = document.createElement("div");
+    div.id = "ErrorDiv";
+    var error = document.createElement("h3");
+    var errorText = document.createTextNode("Nome utente o password errati");
+    error.appendChild(errorText);
+    div.appendChild(error);
+    document.getElementById("loginform").appendChild(div);
+}
+
+//function to detect user type and create proper page
+function userRole(result){
+    const userType = result.user_type;
+    const username = result.username;
+
+    //delete login form and error div
+    var form = document.getElementById("loginform");
+    form.remove();
+
+    //adding new element based on user type
+    var container = document.createElement("div");
+    var role = document.createElement("h3");
+    var roleText = document.createTextNode(userType);
+    var welcomeMsg = document.createElement("h2");
+    var welcomeMsgText = document.createTextNode("Benvenuto " + username);
+    welcomeMsg.appendChild(welcomeMsgText);
+    container.appendChild(welcomeMsg);
+    role.appendChild(roleText);
+    container.appendChild(role);
+    document.body.appendChild(container);
 }
