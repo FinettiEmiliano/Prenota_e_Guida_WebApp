3a39839fb06f3354bc9f95b668a2ee99b8816817
jacopo-bennati <jacopo.bennati@studenti.unitn.it>
2022-05-26 20:02:16 +0200

    added user-availability relation (db)

--- app/models/availability.model.js
+++ app/models/availability.model.js
@@ -1,4 +1,7 @@
 var mongoose = require('mongoose');
+const db = require('./db.model');
+const User = require('./availability.model');
+
 const Date = new mongoose.Schema({
     day: Number,
     month: Number,
@@ -13,15 +16,16 @@ module.exports = mongoose.model(
     "availability",
     new mongoose.Schema({
         date: Date,
-        instructor_username: {
-            type: String,
-            required: true
+        instructor: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+            //required: true
         },
         start_time: Time,
         end_time: Time,
         duration: {
             type: Number,
-            required: true
+            //required: true
         },
         time_slots: [Time]
     }, { timestamps: true }),
