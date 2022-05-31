53b3cbd9e7bd0a035f709e7491e1e6a041d12b7f
FinettiEmiliano <emiliano.finetti@studenti.unitn.it>
2022-05-31 16:54:10 +0200

    Implemented REST API

---
+++ app/controllers/availabilities.controller.js
@@ -0,0 +1,206 @@
+const Workshift = require('../models/availability.model'); // get out availability model
+const User = require('../models/user.model'); // get out user model
+
+//admin settings --> to implement in a following user story
+const opening_hour = 8;
+const opening_minute = 0;
+const closing_hour = 20;
+const closing_minute = 0;
+
+exports.create = async(req, res) => {
+
+    //check if there are some empty or incorrect field in the request
+    if (isCorrect(req))
+    return res.status(412).json({ success: false, message: "Some required filed are emtpy or incorrect" });
+
+    //check instructor
+    let instructor = await User.findOne({_id : req.body.instructor }).exec();
+    if(instructor == null)
+        return res.status(404).json({ success: false, message: "The user doesn't exists" });
+    else if(instructor.user_type != "Istruttore")
+        return res.status(403).json({ success: false, message: "The user isn't an instructor" });
+    
+    //check if the shift goes beyond working hours
+    if (isBeyond(req))
+        return res.status(409).json({ success: false, message: "Shift goes beyond working hours" });
+
+    //overlapping check
+    let overlaps = false;
+    let workshifts = await Workshift.find({ instructor: req.body.instructor }).exec();
+    workshifts.forEach(element => {
+        if(element.instructor == req.body.instructor){
+            if(isOverlapping(req, element, "update")){
+                overlaps = true;
+            }
+        }
+    });
+    if(overlaps)
+        return res.status(476).json({ success: false, message: "The workshift overlaps another workshift" });
+    
+    //divide shift into slots
+    let slots = slotsMaker(req);
+
+    let newWorkshift = new Workshift({
+        date : req.body.date,
+        instructor : req.body.instructor,
+        start_time : req.body.start_time,
+        end_time : req.body.end_time,
+        duration : req.body.duration,
+        time_slots : slots
+    });
+    await newWorkshift.save();
+
+    return res.status(201).json({ success: true, message: "workshift created." });
+}
+
+exports.findAllofInstructor = async(req, res) => {
+
+    let workshifts = await Workshift.find({ instructor: req.params.id }).exec();
+
+    //check if there are workshifts
+    if (workshifts.length == 0)
+        return res.status(204).json({ success: false, message: "There are no workshifts" })
+
+    workshifts = workshifts.map((workshifts) => {
+        return {
+            date : workshifts.date,
+            instructor : workshifts.instructor,
+            start_time : workshifts.start_time,
+            end_time : workshifts.end_time,
+            duration : workshifts.duration,
+            time_slots : workshifts.time_slots
+        };
+    });
+
+    return res.status(200).json({
+        success: true,
+        message: 'OK.',
+        workshifts: workshifts
+    });
+}
+
+exports.update = async(req, res) => {
+
+    let workshift = await Workshift.findById({ _id: req.params.id }).exec();
+
+    //check if the workshift exists
+    if (!workshift)
+        return res.status(400).json({ success: false, message: "The workshift does not exist" })
+
+    //check if there are some empty or incorrect field in the request
+    if (isCorrect(req))
+        return res.status(412).json({ success: false, message: "Some required filed are emtpy or incorrect" });
+
+    //check instructor
+    let instructor = await User.findOne({_id : req.body.instructor }).exec();
+    if(instructor == null)
+        return res.status(404).json({ success: false, message: "The user doesn't exists" });
+    else if(instructor.user_type != "Istruttore")
+        return res.status(403).json({ success: false, message: "The user isn't an instructor" });
+    
+    //check if the shift goes beyond working hours
+    if (isBeyond(req))
+        return res.status(419).json({ success: false, message: "Shift goes beyond working hours" });
+
+    //overlapping check
+    let overlaps = false;
+    let workshifts = await Workshift.find({ instructor: req.body.instructor }).exec();
+    workshifts.forEach(element => {
+        if(element.instructor == req.body.instructor){
+            if(isOverlapping(req, element, "update")){
+                overlaps = true;
+            }
+        }
+    });
+
+    if(overlaps)
+        return res.status(476).json({ success: false, message: "The workshift overlaps another workshift" });
+    
+    //divide shift into slots
+    let slots = slotsMaker(req);
+
+    await Workshift.findByIdAndUpdate(req.params.id, {
+        date : req.body.date,
+        instructor : req.body.instructor,
+        start_time : req.body.start_time,
+        end_time : req.body.end_time,
+        duration : req.body.duration,
+        time_slots : slots
+    });
+
+    return res.status(200).json({ success: true, messagge: "Workshift updated" });
+}
+
+exports.delete = async(req, res) => {
+
+    //check instructor
+    let instructor = await User.findOne({_id : req.query.id }).exec();
+    if(instructor == null)
+        return res.status(404).json({ success: false, message: "The user doesn't exists" });
+    else if(instructor.user_type != "Istruttore")
+        return res.status(403).json({ success: false, message: "The user isn't an instructor" });
+    
+    let workshift = await Workshift.findByIdAndRemove(req.params.id).exec();
+
+    //check if the workshift exist
+    if (!workshift)
+        return res.status(400).json({ success: false, message: "The workshift does not exist" })
+
+    return res.status(200).json({ success: true, messagge: "Cancellation done" });
+}
+
+//function to check if there are some empty or incorrect field in the request
+function isCorrect(req){
+    if (req.body.date.day === 0 || req.body.date.month === 0 || req.body.date.year === 0 ||
+        req.body.date.day === "" || req.body.date.month === "" || req.body.date.year === "" ||
+        req.body.start_time.hour === "" || req.body.start_time.minute === "" ||
+        req.body.end_time.hour === "" || req.body.end_time.minute === "" || 
+        req.body.instructor === "" || req.body.duration === 0 || req.body.duration === "")
+        return true;
+}
+
+//function to check if the shift goes beyond working hours
+function isBeyond(req){
+    let req_start = (req.body.start_time.hour * 60) + req.body.start_time.minute;
+    let req_end = (req.body.end_time.hour * 60) + req.body.end_time.minute;
+    let opening = (opening_hour * 60) + opening_minute;
+    let closing = (closing_hour * 60) + closing_minute;
+
+    if (req_start < opening || req_end > closing)
+        return true;
+}
+
+//overlapping check function
+function isOverlapping(req, element, method){
+    let req_start = (req.body.start_time.hour * 60) + req.body.start_time.minute;
+    let element_start = (element.start_time.hour * 60) + element.start_time.minute;
+    let req_end = (req.body.end_time.hour * 60) + req.body.end_time.minute;
+    let element_end = (element.end_time.hour * 60) + element.end_time.minute;
+
+    if(req.body.date.day == element.date.day && req.body.date.month == element.date.month && req.body.date.year == element.date.year && req.params.id !== element._id){ //date check
+        if((req_start > element_start && req_start < element_end) || //starting time in the middle
+        (req_end > element_start && req_end < element_end) || //ending time in the middle
+        (req_start < element_start && req_end > element_end) || //starting before and ending after
+        (req_start === element_start && req_end === element_end)) //same time
+            return true;
+    }
+    return false
+}
+
+//divide shift in slots
+function slotsMaker(req){
+    let req_start = (req.body.start_time.hour * 60) + req.body.start_time.minute;
+    let req_end = (req.body.end_time.hour * 60) + req.body.end_time.minute;
+    let slotsNumber = Math.floor((req_end - req_start) / req.body.duration);
+    let slots = [];
+
+    for(i=0; i<slotsNumber; i++){
+        let hour = Math.floor((req_start + (req.body.duration * i)) / 60);
+        let minute = ((req_start + (req.body.duration * i)) % 60);
+        slots.push({
+            hour: hour,
+            minute : minute
+        })
+    }
+    return slots;
+}
---
+++ app/test/users.test.js
@@ -0,0 +1,438 @@
+const request = require('supertest');
+const server = require('../../server');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const User = require('../models/user.model');
+const { ObjectId } = require('bson');
+
+beforeAll( async () => { jest.setTimeout(10000);
+    server.db = await mongoose.connect(process.env.DB_URL); });
+afterAll( () => { mongoose.connection.close(true); });
+
+var token = jwt.sign( {username: 'AVirgiliana', id: '628e4bb2f23d519a0b744d9c'},
+    process.env.SUPER_SECRET, {expiresIn: 86400} ); // create a valid tokens
+
+//check if there aren't any user in the database
+test('GET /api/v1/users - DB with no users', async () => {
+
+    await User.deleteMany({user_type: 'Studente'});
+    await User.deleteMany({user_type: 'Istruttore'});
+
+    return request(server)
+    .get('/api/v1/users' + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .expect(204)
+    .expect((res) => {
+        res.body.success = false;
+        res.body.message = 'There are no Users';
+    })
+});
+
+//check DB without students
+test('GET /api/v1/users/students - DB without students', async () => {
+    
+    await User.deleteMany({user_type: 'Studente'});
+
+    return request(server)
+    .get('/api/v1/users/students' + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .expect(204)
+    .expect((res) => {
+        res.body.success = false;
+        res.body.message == 'There are no Students.';
+    })
+});
+
+//check DB without instructors
+test('GET /api/v1/users/instructors - DB without instructors', async () => {
+
+    await User.deleteMany({user_type: 'Istruttore'});
+
+    return request(server)
+    .get('/api/v1/users/instructors' + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .expect(204)
+    .expect((res) => {
+        res.body.success = false;
+        res.body.message == 'There are no Instructors.';
+    })
+});
+
+//check DB with users
+test('GET /api/v1/users - DB with users', async () => {
+
+    await new User({
+        name: 'Studente',
+        surname: 'Prova',
+        user_type: 'Studente',
+        username: 'StudenteProva0',
+        password: 'njids',
+        photo: 'foto.jpg'
+    }).save();
+
+    await new User({
+        name: 'Istruttore',
+        surname: 'Prova',
+        user_type: 'Istruttore',
+        username: 'IstruttoreProva0',
+        password: 'vdsvd',
+        photo: 'foto.jpg'
+    }).save();
+
+    return request(server)
+    .get('/api/v1/users' + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .expect(200)
+    .expect((res) => {
+        res.body.success = true;
+        res.body.message = 'OK';
+        res.body.users = [];
+    })
+            
+});
+
+//check DB with students
+test('GET /api/v1/users/students - DB with students', async () => {
+
+    await new User({
+        name: 'Studente',
+        surname: 'Prova',
+        user_type: 'Studente',
+        username: 'StudenteProva0',
+        password: 'fewd',
+        photo: 'foto.jpg'
+    }).save();
+
+    return request(server)
+    .get('/api/v1/users/students' + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .expect(200)
+    .expect((res) => {
+        res.body.success = true;
+        res.body.message == 'OK';
+        res.body.users = [];
+    })
+});
+
+//check DB with instructors
+test('GET /api/v1/users/instructors - DB with instructors', async () => {
+
+    await new User({
+        name: 'Istruttore',
+        surname: 'Prova',
+        user_type: 'Istruttore',
+        username: 'IstruttoreProva1',
+        password: 'fsqs',
+        photo: 'foto.jpg'
+    }).save();
+
+    return request(server)
+    .get('/api/v1/users/instructors' + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .expect(200)
+    .expect((res) => {
+        res.body.success = true;
+        res.body.message == 'OK';
+        res.body.users = [];
+    })
+});
+
+//check if there isn't name request
+test('POST /api/v1/users - no name', () => {
+    return request(server)
+    .post('/api/v1/users')
+    .set('Content-type', 'application/json')
+    .send({ 
+        'token' : token,
+        'name': '',
+        'surname': 'Bennati',
+        'user_type' : 'Studente',
+        'photo': 'foto.jpg'
+    })
+    .expect(400)
+    .expect((res) => {
+        res.body.success = false;
+        res.body.message = 'Name, surname or user_type undefined.';
+    })       
+});
+
+//check if there isn't surname request
+test('POST /api/v1/users - no surname', () => {
+    return request(server)
+    .post('/api/v1/users')
+    .set('Content-type', 'application/json')
+    .send({ 
+        'token' : token,
+        'name': 'Jacopo',
+        'surname': '',
+        'user_type' : 'Studente',
+        'photo': 'foto.jpg'
+    })
+    .expect(400)
+    .expect((res) => {
+        res.body.success = false;
+        res.body.message = 'Name, surname or user_type undefined.';
+    })    
+});
+
+//check if there isn't user_type request
+test('POST /api/v1/users - no user_type', () => {
+    return request(server)
+    .post('/api/v1/users')
+    .set('Content-type', 'application/json')
+    .send({ 
+        'token' : token,
+        'name': 'Jacopo',
+        'surname': 'Bennati',
+        'user_type' : '',
+        'photo': 'foto.jpg'
+    })
+    .expect(400)
+    .expect((res) => {
+        res.body.success = false;
+        res.body.message = 'Name, surname or user_type undefined.';
+    })    
+});
+
+//check if all fileds are correct
+test('POST /api/v1/users - all correct', () => {
+    return request(server)
+    .post('/api/v1/users')
+    .set('Content-type', 'application/json')
+    .send({ 
+        'token' : token,
+        'name': 'Jacopo',
+        'surname': 'Bennati',
+        'user_type' : 'Studente',
+        'photo': 'foto.jpg'
+    })
+    .expect(201)
+    .expect((res) => {
+        res.body.success = true;
+        res.body.message = 'User created.';
+    })    
+});
+
+//check delete a non existent user
+test('DELETE /api/v1/users/:id - delete non existent user', () => {
+    return request(server)
+    .delete('/api/v1/users/728e4ca86f084f37396d2050' + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .expect(404)
+    .expect((res) => {
+        res.body.success = false;
+        res.body.message == 'The user does not exist';
+    })
+});
+
+//check delete an existent user
+test('DELETE /api/v1/users/:id - delete existent user', async () => {
+
+    let user = await new User({
+        name: 'Studente',
+        surname: 'Prova',
+        user_type: 'Studente',
+        username: 'StudenteProva0',
+        password: 'njids',
+        photo: 'foto.jpg'
+    }).save();
+
+    return request(server)
+    .delete('/api/v1/users/' + user._id + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .expect(200)
+    .expect((res) => {
+        res.body.success = true;
+        res.body.message == 'Cancellation done';
+    })
+});
+
+//check find a non existent user
+test('GET /api/v1/users/:username - search a non existent user', () => {
+    return request(server)
+    .get('/api/v1/users/IamNOTaUSER' + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .expect(404)
+    .expect((res) => {
+        res.body.success = false;
+        res.body.message = 'A User with the specified username was not found.';
+    })
+});
+
+//check find an existent user
+test('GET /api/v1/users/:username - search an existent user', () => {
+    return request(server)
+    .get('/api/v1/users/AVirgiliana' + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .expect(200)
+    .expect((res) => {
+        res.body.success = true;
+        res.body.message = 'OK';
+        res.body.users = [];
+    })
+});
+
+//check update a non existent user
+test('PUT /api/v1/users/:id - update a non existent user', () => {
+    return request(server)
+    .put('/api/v1/users/' + new ObjectId + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .send({ 
+        'name': 'Not',
+        'surname': 'User',
+        'user_type': 'Studente',
+        'username': 'NotUser0',
+        'changePsw': false,
+        'photo': 'foto.jpg'
+    })
+    .expect(404)
+    .expect((res) => {
+        res.body.success = false;
+        res.body.message = 'A user with the specified ID was not found.';
+    })    
+});
+
+//check if there isn't name request
+test('PUT /api/v1/users/:id - update no name', async () => {
+                
+    let student = await User.findOne({ username: "StudenteProva0" });
+    let studentID = student._id.toString();
+    
+    return request(server)
+    .put('/api/v1/users/' + studentID + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .send({ 
+        'name': '',
+        'surname': 'Bennati',
+        'user_type' : 'Studente',
+        'username': 'JacopoBennati0',
+        'changePsw': false,
+        'photo': 'foto.jpg'
+    })
+    .expect(400)
+    .expect((res) => {
+        res.body.success = false;
+        res.body.message = 'Name, surname or user_type undefined.';
+    })    
+});
+
+//check if there isn't surname request
+test('PUT /api/v1/users/:id - update no surname', async () => {
+            
+    let student = await User.findOne({ username: "StudenteProva0" });
+    let studentID = student._id.toString();
+    
+    return request(server)
+    .put('/api/v1/users/' + studentID + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .send({ 
+        'name': 'Jacopo',
+        'surname': '',
+        'user_type' : 'Studente',
+        'username': 'JacopoBennati0',
+        'changePsw': false,
+        'photo': 'foto.jpg'
+    })
+    .expect(400)
+    .expect((res) => {
+        res.body.success = false;
+        res.body.message = 'Name, surname or user_type undefined.';
+    })    
+});
+
+//check if there isn't user_type request
+test('PUT /api/v1/users/:id - update no user_type', async () => {
+        
+    let student = await User.findOne({ username: "StudenteProva0" });
+    let studentID = student._id.toString();
+    
+    return request(server)
+    .put('/api/v1/users/' + studentID + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .send({ 
+        'name': 'Jacopo',
+        'surname': 'Bennati',
+        'user_type' : '',
+        'username': 'JacopoBennati0',
+        'changePsw': false,
+        'photo': 'foto.jpg'
+    })
+    .expect(400)
+    .expect((res) => {
+        res.body.success = false;
+        res.body.message = 'Name, surname or user_type undefined.';
+    })                              
+});
+
+//check if name and surname are same as the previous one
+test('PUT /api/v1/users/:id - update name and surname are same as the previous one', async () => {
+        
+    let student = await User.findOne({ username: "StudenteProva0" });
+    let studentID = student._id.toString();
+    
+    return request(server)
+    .put('/api/v1/users/' + studentID + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .send({ 
+        'name': student.name,
+        'surname': student.surname,
+        'user_type' : student.user_type,
+        'username': student.username,
+        'changePsw': false,
+        'photo': student.photo
+    })
+    .expect(409)
+    .expect((res) => {
+        res.body.success = true;
+        res.body.message = 'User was not updated, name and surname were not changed';
+    })                              
+});
+
+//check change password
+test('PUT /api/v1/users/:id - update only change password', async () => {
+    
+    let student = await User.findOne({ username: "StudenteProva0" });
+    let studentID = student._id.toString();
+    
+    return request(server)
+    .put('/api/v1/users/' + studentID + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .send({ 
+        'name': 'Studente',
+        'surname': 'Prova',
+        'user_type' : 'Studente',
+        'username': 'StudenteProva0',
+        'changePsw': true,
+        'photo': 'foto.jpg'
+    })
+    .expect(200)
+    .expect((res) => {
+        res.body.success = true;
+        res.body.message = 'User updated';
+    })                        
+});
+
+//check correct update
+test('PUT /api/v1/users/:id - update correct user', async () => {
+    
+    let student = await User.findOne({ username: "StudenteProva0" });
+    let studentID = student._id.toString();
+    
+    return request(server)
+    .put('/api/v1/users/' + studentID + '?token=' + token)
+    .set('Content-type', 'application/json')
+    .send({ 
+        'name': 'Giaco',
+        'surname': 'Bennatia',
+        'user_type' : 'Studente',
+        'username': 'JacopoBennati0',
+        'changePsw': false,
+        'photo': 'foto.jpg'
+    })
+    .expect(200)
+    .expect((res) => {
+        res.body.success = true;
+        res.body.message = 'User updated';
+    })                  
+});
+
